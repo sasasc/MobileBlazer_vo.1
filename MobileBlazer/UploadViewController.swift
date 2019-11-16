eb93c978122e047428e87424f555cd82580ac5d0
rama naidu <mramachandra810@gmail.com>
2019-11-16 23:21:53 +0530

    implements upload images feature

---
+++ MobileBlazer/UploadViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UploadViewController.swift
+//  MobileBlazer
+//
+//  Created by ramachandra on 16/11/19.
+//  Copyright © 2019 ramachandra. All rights reserved.
+//
+
+import UIKit
+
+class UploadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MobileBlazer/ViewController.swift
+++ MobileBlazer/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let uiVc = UploadViewController()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
